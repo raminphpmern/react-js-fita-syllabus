742c159f60a9421dc5ccd8b913808297a46e4565
Ramkumar <ramkumar87.bca@gmail.com>
2024-08-20 19:38:12 +0530

    Create operators-sample.js

---
+++ operators-sample.js
@@ -0,0 +1,9 @@
+// Increment Operator
+let g = 2;
+console.log("first g value is",g);
+g1 = g++;
+console.log("second g value is",g);
+g2 = ++g;
+console.log("third g value is",g);
+console.log("first g1 value is",g1);
+console.log("first g2 value is",g2);
