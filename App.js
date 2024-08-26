1655a19800c0bf28a6582a8944b0cc1156bde847
Ramkumar <ramkumar87.bca@gmail.com>
2024-08-26 13:15:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+      <div class="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+        <div style={{paddingLeft: "16px"}}>
+          <h2>Top Navigation Example</h2>
+          <p>Some content..</p>
+        </div>
+      </div>
+          
+    
+    
+    </>
+  
+  );
+}
+
+export default App;
