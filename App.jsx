bf556a56adcca923b1c30242de93b3974e57fb1e
Ramkumar <ramkumar87.bca@gmail.com>
2024-09-14 17:21:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Pages/Layout";
+import Home from "./Components/Pages/Home";
+import Blogs from "./Components/Pages/Blogs";
+import Contact from "./Components/Pages/Contact";
+import Car from "./Components/CarComps";
+import NoPage from "./Components/Pages/NoPage";
+import SampleCounter from "./Components/SampleCounter";
+import MyPropsSample from "./Components/MyPropsSample";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="blogs" element={<Blogs />} />
+    //       <Route path="contact" element={<Contact />} />
+    //       <Route path="updateobjects" element={<Car />} />
+    //       <Route path="*" element={<NoPage />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <>
+      <BrowserRouter>
+         <Routes>
+           <Route path="/" element={<Layout />}>
+             <Route index element={<Home />} />
+             <Route path="blogs" element={<Blogs />} />
+             <Route path="contact" element={<Contact />} />
+             <Route path="updateobjects" element={<Car />} />
+             <Route path="*" element={<NoPage />} />
+           </Route>
+         </Routes>
+      </BrowserRouter>
+      <SampleCounter></SampleCounter><MyPropsSample /></>
+  );
+}
+
+export default App;
