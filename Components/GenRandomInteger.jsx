40a39d9c5a7a3a6952c0128f767ad1469990d774
Ramkumar <ramkumar87.bca@gmail.com>
2024-09-18 11:46:07 +0530

    Add files via upload

---
+++ Components/GenRandomInteger.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+
+function GenRandomInteger() {
+    const [click, setClick] = useState([]);
+
+    const addNumber = () => {
+        setClick([
+            ...click,
+            {
+                id: click.length,
+                value: Math.random() * 10
+            }
+        ]);
+    };
+
+    return (
+        <div>
+            <ul>
+                {click.map(item => (
+                    <li key={item.id}>{item.value}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}>
+                Click me
+            </button>
+        </div>
+    );
+}
+
+export default GenRandomInteger;
