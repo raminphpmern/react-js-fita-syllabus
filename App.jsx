43def1f5b224b7a4c66ea6c835bea0ea88ef774e
Ramkumar <ramkumar87.bca@gmail.com>
2024-09-27 20:42:56 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+import { createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Pages/Layout";
 import Home from "./Components/Pages/Home";
@@ -8,24 +9,17 @@ import NoPage from "./Components/Pages/NoPage";
 import SampleCounter from "./Components/SampleCounter";
 import MyPropsSample from "./Components/MyPropsSample";
 import GenRandomInteger from "./Components/GenRandomInteger";
-
+import CounterApp from "./Components/CounterApp";
+import SampleListData from "./Components/SampleListData";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Component1 from "./Components/Component1";
+import Component3 from "./Components/Component3";
 
+const Context = createContext();
 const App = () => {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Layout />}>
-    //       <Route index element={<Home />} />
-    //       <Route path="blogs" element={<Blogs />} />
-    //       <Route path="contact" element={<Contact />} />
-    //       <Route path="updateobjects" element={<Car />} />
-    //       <Route path="*" element={<NoPage />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-    
-      <>
+    <>
+{/*     
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -36,7 +30,18 @@ const App = () => {
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
-      </BrowserRouter><SampleCounter></SampleCounter><MyPropsSample /><GenRandomInteger /></>
+      </BrowserRouter> */}
+      {/* <CounterApp></CounterApp> */}
+      <Context.Provider value={{ data: "Data from context!" }}>
+        <Component1></Component1>
+        <Component3></Component3>
+      </Context.Provider>
+      <Component3></Component3>
+      {/* <SampleListData /> */}
+      {/* <SampleCounter></SampleCounter><MyPropsSample /><GenRandomInteger /> */}
+      
+    
+      </>
   );
 }
 
