80ce798d3818de6b233195da80edaaf10a24becf
Ramkumar <ramkumar87.bca@gmail.com>
2024-10-22 20:38:57 +0530

    Add files via upload
    Sample React App with Basic Redux Toolkit App a single reducer

---
+++ sample-redux-toolkit-to-do-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Task from './components/Task';
+import TaskList from './components/TaskList';
+function App() {
+  return (
+    <div className="App">
+      <Task />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
